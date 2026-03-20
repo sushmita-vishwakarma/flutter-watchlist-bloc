69d16efa7fbeeb73b996ec47595934c2b399b19c
Sushmita Vishwakarma <153200950+sushmita-vishwakarma@users.noreply.github.com>
2026-03-20 15:23:49 +0530

    Create stock.dart

---
+++ lib/watchlist/model/stock.dart
@@ -0,0 +1,13 @@
+class Stock {
+  final String name;
+  final String exchange;
+  final double price;
+  final double change;
+
+  Stock({
+    required this.name,
+    required this.exchange,
+    required this.price,
+    required this.change,
+  });
+}
